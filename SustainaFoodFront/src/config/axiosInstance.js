f0a55aee4308a7e1930965e37950fd1d43e83404
alasalah123 <alaeddine.salah@esprit.tn>
2025-04-29 21:26:24 +0100

    Fix connection between front back rendre

--- SustainaFoodFront/src/config/axiosInstance.js
+++ SustainaFoodFront/src/config/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8082/api", // Change this to your API URL
+    //baseURL: "http://localhost:8082/api", // Change this to your API URL
+    baseURL: import.meta.env.VITE_API_BASE_URL,  // ✅ dynamic based on environment
     headers: { 
         "Content-Type": "application/json"
     },
