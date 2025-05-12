dba27c7cac807b7f73c7300ead6e810b4abbb62a
chammem sinda <99534039+chammem@users.noreply.github.com>
2025-05-12 02:30:39 +0100

    errur front 4

--- SustainaFoodFront/vite.config.js
+++ SustainaFoodFront/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-  '/api': 'http://127.0.0.1:8082', // Proxy API requests to the backend
+  '/api': 'http:/localhost:8082', // Proxy API requests to the backend
   '/socket.io': {
-    target: 'http://127.0.0.1:8082', // Proxy WebSocket connections to the backend
+    target: 'http://localhost:8082', // Proxy WebSocket connections to the backend
     ws: true,
     changeOrigin: true, // Ensure the origin header is updated
   },
